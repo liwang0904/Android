7fa3d061bbffb509b4fccd53597338cd08240ed1
Lindsey <lindsey.xixi.wang@gmail.com>
2019-11-02 10:34:05 -0400

    Dice Roller and Trivia

---
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,37 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_title, container, false
+        )
+        binding.playButton.setOnClickListener {v: View ->
+            v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment22())
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
