7c0670dda24204551a23d5fb3e9d641203a72aee
Lindsey <lindsey.xixi.wang@gmail.com>
2019-12-30 20:28:42 -0500

    Sleep Tracker and RecyclerView

---
+++ andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {}
+}
