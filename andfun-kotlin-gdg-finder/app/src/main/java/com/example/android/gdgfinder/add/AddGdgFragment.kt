32a011fe212a137759006d42727aa59f1402e9f1
Lindsey <lindsey.xixi.wang@gmail.com>
2020-01-12 19:08:38 -0500

    GDG Finder

--- andfun-kotlin-gdg-finder/app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ andfun-kotlin-gdg-finder/app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -35,6 +35,9 @@ class AddGdgFragment : Fragment() {
                     Snackbar.LENGTH_SHORT // How long to display the message.
                 ).show()
                 viewModel.doneShowingSnackbar()
+
+                binding.button.contentDescription=getString(R.string.submitted)
+                binding.button.text=getString(R.string.done)
             }
         })
 
