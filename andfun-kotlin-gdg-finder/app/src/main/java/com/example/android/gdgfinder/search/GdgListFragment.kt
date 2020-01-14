b6cef99e49415c6581dc7c7447bfc6c6fa03884d
Lindsey <lindsey.xixi.wang@gmail.com>
2020-01-13 21:03:14 -0500

    Update g6g

--- andfun-kotlin-gdg-finder/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ andfun-kotlin-gdg-finder/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -13,7 +13,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
+import com.example.android.gdgfinder.R
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -53,6 +55,30 @@ class GdgListFragment : Fragment() {
                         "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
                         Snackbar.LENGTH_LONG
                     ).show()
+                }
+            }
+        })
+
+        viewModel.regionList.observe(viewLifecycleOwner, object: Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                val children = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+
+                chipGroup.removeAllViews()
+
+                for (chip in children) {
+                    chipGroup.addView(chip)
                 }
             }
         })
@@ -134,5 +160,3 @@ class GdgListFragment : Fragment() {
         }
     }
 }
-
-
