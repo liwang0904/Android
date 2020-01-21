c1ddb7b4212a0b055c15a0e6bd664d6757c406d6
Lindsey <lindsey.xixi.wang@gmail.com>
2020-01-21 17:18:04 -0500

    Notifications

--- andfun-kotlin-gdg-finder/app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
+++ andfun-kotlin-gdg-finder/app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         setupNavigation()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        
+
     }
 
     /**
