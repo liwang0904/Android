0b21f0f11dbd3de85e2288268068a1641e18d00e
Lindsey <lindsey.xixi.wang@gmail.com>
2020-01-21 21:02:26 -0500

    Update Notifications

--- android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -118,11 +118,11 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 }
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
-                val notificationManager = ContextCompat.getSystemService(
-                    app,
-                    NotificationManager::class.java
-                ) as NotificationManager
-                notificationManager.sendNotification(app.getString(R.string.timer_running), app)
+//                val notificationManager = ContextCompat.getSystemService(
+//                    app,
+//                    NotificationManager::class.java
+//                ) as NotificationManager
+//                notificationManager.sendNotification(app.getString(R.string.timer_running), app)
 
                 // TODO: Step 1.15 call cancel notification
 
