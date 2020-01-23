8abfadf4eb827ce9d3e0112eea3b7a099a983146
Lindsey <lindsey.xixi.wang@gmail.com>
2020-01-22 21:22:35 -0500

    update

--- android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -39,24 +39,27 @@ private val FLAGS = 0
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     // Create the content intent for the notification, which launches
     // this activity
-    // TODO: Step 1.11 create intent
+     val contentIntent = Intent(applicationContext, MainActivity::class.java)
 
-    // TODO: Step 1.12 create PendingIntent
-
+     val contentPendingIntent = PendingIntent.getActivity(
+         applicationContext,
+         NOTIFICATION_ID,
+         contentIntent,
+         PendingIntent.FLAG_UPDATE_CURRENT
+     )
     // TODO: Step 2.0 add style
 
     // TODO: Step 2.2 add snooze action
 
-    val builder = NotificationCompat.Builder(
-        applicationContext,
-        applicationContext.getString(R.string.egg_notification_channel_id)
-    ).setSmallIcon(R.drawable.cooked_egg)
-        .setContentTitle(applicationContext
-            .getString(R.string.notification_title))
-        .setContentText(messageBody)
-
-    // TODO: Step 1.13 set content intent
-
+     val builder = NotificationCompat.Builder(applicationContext,
+         applicationContext.getString(R.string.egg_notification_channel_id)
+     )
+         .setSmallIcon(R.drawable.cooked_egg)
+         .setContentTitle(applicationContext.getString(R.string.notification_title))
+         .setContentText(messageBody)
+         // TODO: Step 1.13 set content intent
+         .setContentIntent(contentPendingIntent)
+         .setAutoCancel(true)
         // TODO: Step 2.1 add style to builder
 
         // TODO: Step 2.3 add snooze action
