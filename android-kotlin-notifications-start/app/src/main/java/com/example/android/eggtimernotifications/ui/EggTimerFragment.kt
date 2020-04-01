7080046e9f8b3b82962a22481dd3f2e669bc2ca9
Lindsey <lindsey.xixi.wang@gmail.com>
2020-04-01 16:22:34 -0400

    Dice Roller (REDO)

--- android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -60,6 +60,8 @@ class EggTimerFragment : Fragment() {
             getString(R.string.breakfast_notification_channel_name)
         )
 
+        subscribeTopic()
+
         return binding.root
     }
 
@@ -83,6 +85,19 @@ class EggTimerFragment : Fragment() {
             )
             notificationManager.createNotificationChannel(notificationChannel)
         }
+    }
+
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
     }
 
     companion object {
