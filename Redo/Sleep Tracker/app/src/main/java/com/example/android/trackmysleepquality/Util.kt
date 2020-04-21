0c9878cb09cb8a4a9009f22a02577e2e0ba83218
Lindsey <lindsey.xixi.wang@gmail.com>
2020-04-20 20:32:42 -0400

    Sleep Tracker

--- Redo/Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ Redo/Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -18,6 +18,11 @@ package com.example.android.trackmysleepquality
 
 import android.annotation.SuppressLint
 import android.content.res.Resources
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import androidx.core.text.HtmlCompat
+import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
 
 /**
@@ -56,45 +61,32 @@ fun convertLongToDateString(systemTime: Long): String {
             .format(systemTime).toString()
 }
 
-/**
- * Takes a list of SleepNights and converts and formats it into one string for display.
- *
- * For display in a TextView, we have to supply one string, and styles are per TextView, not
- * applicable per word. So, we build a formatted string using HTML. This is handy, but we will
- * learn a better way of displaying this data in a future lesson.
- *
- * @param   nights - List of all SleepNights in the database.
- * @param   resources - Resources object for all the resources defined for our app.
- *
- * @return  Spanned - An interface for text that has formatting attached to it.
- *           See: https://developer.android.com/reference/android/text/Spanned
- */
-//fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
-//    val sb = StringBuilder()
-//    sb.apply {
-//        append(resources.getString(R.string.title))
-//        nights.forEach {
-//            append("<br>")
-//            append(resources.getString(R.string.start_time))
-//            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
-//            if (it.endTimeMilli != it.startTimeMilli) {
-//                append(resources.getString(R.string.end_time))
-//                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
-//                append(resources.getString(R.string.quality))
-//                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
-//                append(resources.getString(R.string.hours_slept))
-//                // Hours
-//                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
-//                // Minutes
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
-//                // Seconds
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
-//            }
-//        }
-//    }
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
-//    } else {
-//        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
-//    }
-//}
+fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
+    val sb = StringBuilder()
+    sb.apply {
+        append(resources.getString(R.string.title))
+        nights.forEach {
+            append("<br>")
+            append(resources.getString(R.string.start_time))
+            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
+            if (it.endTimeMilli != it.startTimeMilli) {
+                append(resources.getString(R.string.end_time))
+                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
+                append(resources.getString(R.string.quality))
+                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
+                append(resources.getString(R.string.hours_slept))
+                // Hours
+                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
+                // Minutes
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
+                // Seconds
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
+            }
+        }
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+}
--- Redo/Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ Redo/Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -22,8 +22,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityBinding
+import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModel
+import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModelFactory
 
 /**
  * Fragment that displays a list of clickable icons,
@@ -46,6 +50,18 @@ class SleepQualityFragment : Fragment() {
                 inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel =
+                ViewModelProviders.of(
+                        this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        binding.setLifecycleOwner(this)
 
         return binding.root
     }
