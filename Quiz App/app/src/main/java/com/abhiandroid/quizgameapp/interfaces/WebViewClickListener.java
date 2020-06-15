3a5096ce0fef2fe6b800836eb246d100a9fef3d0
Lindsey <lindsey.xixi.wang@gmail.com>
2020-06-14 22:02:59 -0400

    Quiz App

--- Quiz App/app/src/main/java/com/abhiandroid/quizgameapp/interfaces/WebViewClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.abhiandroid.quizgameapp.interfaces;
-
-/**
- * Developed by AbhiAndroid.com
- */
-
-public interface WebViewClickListener {
-    public void onClick();
-}
--- Quiz App/app/src/main/java/com/abhiandroid/quizgameapp/model/Terms_pojo.java
+++
@@ -1,9 +0,0 @@
-package com.abhiandroid.quizgameapp.model;
-
-/**
- * Developed by AbhiAndroid.com
- */
-
-public class Terms_pojo {
-    public String   terms;
-}
